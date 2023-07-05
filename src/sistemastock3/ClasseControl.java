fb8bce2c9fe09e5972d15ebbb430b2b1a48ed5bb
Mauro1Amone <maurosueia2@gmail.com>
2023-07-05 12:25:19 +0200

    Actualização da organização do Codigo

--- src/sistemastock3/ClasseControl.java
+++ src/sistemastock3/ClasseControl.java
@@ -1,108 +1,19 @@
-package sistemastock3;
+ package sistemastock3;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class ClasseControl {
 
-    //Declaração de uma Lista 
-    private List<Produto> produtos;
-
-    public ClasseControl() {
-        //Inicialização de um arraylist
-        produtos = new ArrayList<>();
-    }
-
-    //Metódo para registar um produto
-    public void registarProduto(String nome, double preco, int quantidade) {
-        //verifica se tudo vem da forma correcta
-        try {
-            Produto produto = new Produto(nome, preco, quantidade);
-            produtos.add(produto);
-            System.out.println("Produto registrado com sucesso!");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Erro ao registrar o produto: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Ocorreu um erro inesperado ao registrar o produto.");
-        }
-    }
-
-    //Metódo para Editar um produto
-    public void editarProduto(int numeroProduto, String nome, double preco, int quantidade) {
-        //verifica se o numero do produto e menor que zero e se o numero do produto e maior que a lista
-        if (numeroProduto < 0 || numeroProduto >= produtos.size()) {
-            System.out.println("Número de produto invalido!");
-            return;
-        }
-
-        //Recebe os novos valores
-        Produto produto = produtos.get(numeroProduto);
-        produto.setNome(nome);
-        produto.setPreco(preco);
-        produto.setQuantidade(quantidade);
-        System.out.println("Produto editado com sucesso!");
-    }
-
-    //Metódo para Remover um produto
-    public void removerProduto(int numeroProduto) {
-        if (numeroProduto < 0 || numeroProduto >= produtos.size()) {
-            System.out.println("Número de produto invalido!");
-            return;
-        }
-
-        Produto produto = produtos.get(numeroProduto);
-        produtos.remove(produto);
-        System.out.println("Produto removido com sucesso!");
-    }
-
-    //Metódo para Mostrar todos os produtos 
-    public void MostrarStock() {
-        System.out.println("Stock Actual:");
-        //Loop para imprimir os produtos
-        for (int i = 0; i < produtos.size(); i++) {
-            Produto produto = produtos.get(i);
-            System.out.println("Produto n:" + i );
-            System.out.println("Nome: " + produto.getNome());
-            System.out.println("Preço: " + produto.getPreco());
-            System.out.println("Quantidade: " + produto.getQuantidade());
-            System.out.println("------------------------");
-        }
-    }
-
-    //Metódo para Registar uma venda     
-    public void registarVenda(int numeroProduto, int quantidadeVendida) {
-        if (numeroProduto < 0 || numeroProduto >= produtos.size()) {
-            System.out.println("Número de produto inválido!!");
-            return;
-        }
-
-        Produto produto = produtos.get(numeroProduto);
-        int quantidadeActual = produto.getQuantidade();
-
-        //Verifica se a quantidade actual e menor que quantidade que se quer vender
-        if (quantidadeActual < quantidadeVendida) {
-            System.out.println("Stock insuficiente!");
-            return;
-        }
-
-        //recebe a nova quantidade 
-        produto.setQuantidade(quantidadeActual - quantidadeVendida);
-        System.out.println("Venda registada com sucesso!");
-    }
-
-    public static void main(String[] args) {
-
-        //Parte onde será inicializado o programa.
-        exibirMenu();//metódo que exibe o menu do programa
-    }
-
     //Metódo para exibir o as opções do menu
     public static void exibirMenu() {
+        
+            Produto produto = new Produto();
+
 
         //Essa variavel define se queremos continuar a execução do programa   
         boolean executando = true;
-        ClasseControl ClasseControl = new ClasseControl();
+       
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("=================================================================================================================");
@@ -132,12 +43,12 @@ public class ClasseControl {
                         System.out.print("Digite a quantidade: ");
                         int quantidade = Integer.parseInt(scanner.nextLine());
                         //esse metodo vai receber por parametros o nome,preco e a quantidade. 
-                        ClasseControl.registarProduto(nome, preco, quantidade);
+                        produto.registarProduto(nome, preco, quantidade);
                         break;
                     }
                     case 2: {
 
-                        ClasseControl.MostrarStock();//Este metodo vai exibir todos os produtos
+                        produto.MostrarStock();//Este metodo vai exibir todos os produtos
                         System.out.print("Digite o número do produto para editar: ");
                         int numeroProduto = Integer.parseInt(scanner.nextLine());
                         System.out.print("Digite o novo nome: ");
@@ -147,27 +58,27 @@ public class ClasseControl {
                         System.out.print("Digite a nova quantidade: ");
                         int novaQuantidade = Integer.parseInt(scanner.nextLine());
                         //Vai passar por parametros os novos valores para o metodo editarProduto
-                        ClasseControl.editarProduto(numeroProduto, novoNome, preco, novaQuantidade);
+                        produto.editarProduto(numeroProduto, novoNome, preco, novaQuantidade);
                         break;
                     }
                     case 3: {
                         //Meodo para remover um produto
-                        ClasseControl.MostrarStock();
+                        produto.MostrarStock();
                         System.out.print("Digite o número do produto para remover: ");
                         int numeroProduto = Integer.parseInt(scanner.nextLine());
-                        ClasseControl.removerProduto(numeroProduto);
+                        produto.removerProduto(numeroProduto);
                         break;
                     }
                     case 4:
-                        ClasseControl.MostrarStock();//exibe os produtos
+                        produto.MostrarStock();//exibe os produtos
                         break;
                     case 5: {
-                        ClasseControl.MostrarStock();//Exibe todos os produtos
+                        produto.MostrarStock();//Exibe todos os produtos
                         System.out.print("Digite o número do produto: ");
                         int numeroProduto = Integer.parseInt(scanner.nextLine());
                         System.out.print("Digite a quantidade a vender: ");
                         int quantidadeVenda = Integer.parseInt(scanner.nextLine());
-                        ClasseControl.registarVenda(numeroProduto, quantidadeVenda);
+                        produto.registarVenda(numeroProduto, quantidadeVenda);
                         break;
                     }
                     case 6:
@@ -189,4 +100,12 @@ public class ClasseControl {
 
         scanner.close();
     }
+    
+    
+    public static void main(String[] args) {
+
+        //Parte onde será inicializado o programa.
+        exibirMenu();//metódo que exibe o menu do programa
+    }
+
 }
--- src/sistemastock3/Produto.java
+++ src/sistemastock3/Produto.java
@@ -1,11 +1,12 @@
 package sistemastock3;
 
+//Classe Produto 
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-//Classe Produto 
 class Produto {
+
 
     //Variaveis ou atributos da classe produto
     private String nome;
@@ -42,5 +43,92 @@ class Produto {
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
+    
+       private List<Produto> produtos;
 
+       
+    public Produto() {
+        //Inicialização de um arraylist
+        produtos = new ArrayList<>();
+    }
+    
+    
+ 
+    //Metódo para Editar um registrar   
+ public  void registarProduto(String nome, double preco, int quantidade) {
+        //verifica se tudo vem da forma correcta
+        try {
+            Produto produto = new Produto(nome, preco, quantidade);
+            produtos.add(produto);
+            System.out.println("Produto registrado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao registrar o produto: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro inesperado ao registrar o produto.");
+        }
+    }
+ 
+ 
+    //Metódo para Editar um produto
+   public  void editarProduto(int numeroProduto, String nome, double preco, int quantidade) {
+        //verifica se o numero do produto e menor que zero e se o numero do produto e maior que a lista
+        if (numeroProduto < 0 || numeroProduto >= produtos.size()) {
+            System.out.println("Número de produto invalido!");
+            return;
+        }
+   }
+   
+   
+   
+    //Metódo para Remover um produto
+    public void removerProduto(int numeroProduto) {
+        if (numeroProduto < 0 || numeroProduto >= produtos.size()) {
+            System.out.println("Número de produto invalido!");
+            return;
+        }
+
+        Produto produto = produtos.get(numeroProduto);
+        produtos.remove(produto);
+        System.out.println("Produto removido com sucesso!");
+    }
+    
+    
+    //Metódo para Registar uma venda     
+    public void registarVenda(int numeroProduto, int quantidadeVendida) {
+        if (numeroProduto < 0 || numeroProduto >= produtos.size()) {
+            System.out.println("Número de produto inválido!!");
+            return;
+        }
+
+        Produto produto = produtos.get(numeroProduto);
+        int quantidadeActual = produto.getQuantidade();
+
+        //Verifica se a quantidade actual e menor que quantidade que se quer vender
+        if (quantidadeActual < quantidadeVendida) {
+            System.out.println("Stock insuficiente!");
+            return;
+        }
+
+        //recebe a nova quantidade 
+        produto.setQuantidade(quantidadeActual - quantidadeVendida);
+        System.out.println("Venda registada com sucesso!");
+    }
+    
+    //Metódo para Mostrar todos os produtos 
+    public void MostrarStock() {
+        System.out.println("Stock Actual:");
+        //Loop para imprimir os produtos
+        for (int i = 0; i < produtos.size(); i++) {
+            Produto produto = produtos.get(i);
+            System.out.println("Produto n:" + i );
+            System.out.println("Nome: " + produto.getNome());
+            System.out.println("Preço: " + produto.getPreco());
+            System.out.println("Quantidade: " + produto.getQuantidade());
+            System.out.println("------------------------");
+        }
+    }
+
+ 
+ 
+ 
 }
